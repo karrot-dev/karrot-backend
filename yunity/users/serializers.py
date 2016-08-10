ce15884acdb19a9354243c6d2cfa61ce76273366
Tilmann Becker <tilmann.becker@gmail.com>
2016-08-10 19:35:31 +0200

    WIP: serializer for versatileimagefield

--- yunity/users/serializers.py
+++ yunity/users/serializers.py
@@ -1,15 +1,32 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework.relations import PrimaryKeyRelatedField
+from versatileimagefield.serializers import VersatileImageFieldSerializer
+from yunity.users.models import ProfilePicture as ProfilePictureModel
+
+class ProfilePictureSerializer(serializers.ModelSerializer):
+    image = VersatileImageFieldSerializer(
+        # defined in config/versatileimagefield.py
+        sizes='profile_image'
+    )
+
+    class Meta:
+        model = ProfilePictureModel
+        fields = ['image', ]
 
 
 class UserSerializer(serializers.ModelSerializer):
+    profile_picture = ProfilePictureSerializer(allow_null=True, required=False)
 
     class Meta:
         model = get_user_model()
         fields = ['id', 'display_name', 'first_name', 'last_name', 'email', 'password',
-                  'address', 'latitude', 'longitude']
+                  'address', 'latitude', 'longitude', 'profile_picture']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        return self.Meta.model.objects.create_user(
-            **{x: validated_data.get(x, None) for x in self.get_fields() if x is not 'id'})
+        picture_data = validated_data.pop('profile_picture', None)
+        user = self.Meta.model.objects.create_user(**validated_data)
+        if picture_data:
+            ProfilePictureModel.objects.create(user=user, **picture_data)
+        return user
