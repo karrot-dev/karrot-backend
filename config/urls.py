6baa704700fa438f508bb089c52bea560cfd5a3e
id-gue <gue@danke.fish>
2017-08-03 10:21:29 +0200

    minor changes in url routers

--- config/urls.py
+++ config/urls.py
@@ -58,7 +58,7 @@ router.register('invitations', InvitationsViewSet)
 router.register('invitations', InvitationAcceptViewSet)
 
 # Feedback endpoints
-router.register(r'feedback', FeedbackViewSet, base_name='feedback-messages')
+router.register(r'feedback', FeedbackViewSet, base_name='user_feedback')
 
 urlpatterns = [
     url(r'^api/', include(router.urls, namespace='api')),
