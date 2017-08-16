92f8b98a31a489b77752bf6a5260657072c50f64
id-gue <gue@danke.fish>
2017-08-16 09:59:52 +0200

    flake8 corrections

--- foodsaving/stores/serializers.py
+++ foodsaving/stores/serializers.py
@@ -244,13 +244,13 @@ class FeedbackSerializer(serializers.ModelSerializer):
             'given_by': {'read_only': True}
         }
         """
-    """  
+    """
     def validate_given_by(self, user_id):
         if user_id not in self.context['request'].user.groups.all():
             raise serializers.ValidationError(_('You are not member of the store\'s group.'))
         return user_id
     """
-    
+
     def validate_about(self, pickup_dates_id):
         if pickup_dates_id not in self.context['request'].user.pickup_dates.all():
             raise serializers.ValidationError(_('You aren\'t assign to the pickup.'))
