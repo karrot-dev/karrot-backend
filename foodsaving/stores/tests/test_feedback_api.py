a622ed1918f234b6126983b522444dc831b99341
mddemarie <mddemarie@gmail.com>
2017-08-31 13:02:37 +0200

    1 more test for the feedback in the future + comment clean-up

--- foodsaving/stores/tests/test_feedback_api.py
+++ foodsaving/stores/tests/test_feedback_api.py
@@ -8,8 +8,6 @@ from foodsaving.stores.factories import StoreFactory, PickupDateFactory
 from foodsaving.users.factories import UserFactory
 from foodsaving.stores.models import Feedback
 
-# from foodsaving.stores.models import Feedback
-
 
 class FeedbackTest(APITestCase):
     @classmethod
@@ -17,28 +15,18 @@ class FeedbackTest(APITestCase):
         super().setUpClass()
         cls.url = '/api/feedback/'
 
-        """
-        pickup date for group with one member and one store
-
-        setup:
-        1. create user
-        2. create group with the user as member
-        3. create a store within that group
-        4. create a pickup within that store
-        """
-
         cls.member = UserFactory()
         cls.collector = UserFactory()
-        # create group and let 'member' and 'collector' join
         cls.group = GroupFactory(members=[cls.member, cls.collector])
         cls.store = StoreFactory(group=cls.group)
-        cls.pickup = PickupDateFactory(store=cls.store)
+        cls.pickup = PickupDateFactory(store=cls.store, date=timezone.now() + relativedelta(days=1))
 
         # not a member of the group
         cls.user = UserFactory()
 
         # past pickup date
         cls.past_pickup = PickupDateFactory(store=cls.store, date=timezone.now() - relativedelta(days=1))
+
         # transforms the menber into a collector
         cls.past_pickup.collectors.add(cls.collector)
 
@@ -46,6 +34,14 @@ class FeedbackTest(APITestCase):
         cls.feedback_post = {
             'given_by': cls.collector.id,
             'about': cls.past_pickup.id,
+            'weight': 2,
+            'comment': 'asfjk'
+        }
+
+        # create a feedback to future pickup
+        cls.future_feedback_post = {
+            'given_by': cls.collector.id,
+            'about': cls.pickup.id,
             'weight': 2,
             'comment': 'asfjk'
         }
@@ -67,10 +63,6 @@ class FeedbackTest(APITestCase):
     def test_create_feedback_fails_as_non_user(self):
         """
         Non-User is not allowed to give feedback.
-
-        Test:
-        1. user gives feedback to pickup
-        2. make sure that response is NOT valid
         """
         response = self.client.post(self.url, self.feedback_post, format='json')
         self.assertEqual(
@@ -79,11 +71,6 @@ class FeedbackTest(APITestCase):
     def test_create_feedback_fails_as_non_group_member(self):
         """
         User is not allowed to give feedback when not a member of the stores group.
-
-        Test:
-        1. log user in
-        2. user gives feedback to pickup
-        3. make sure that response is NOT valid
         """
         self.client.force_login(user=self.user)
         response = self.client.post(self.url, self.feedback_post, format='json')
@@ -94,11 +81,6 @@ class FeedbackTest(APITestCase):
         """
         Group Member is not allowed to give feedback when he is not assiged to the
         Pickup.
-
-        Test:
-        1. log user in as group member
-        2. user gives feedback to pickup
-        3. feedback NOT created
         """
         self.client.force_login(user=self.member)
         response = self.client.post(self.url, self.feedback_post, format='json')
@@ -108,11 +90,6 @@ class FeedbackTest(APITestCase):
     def test_create_feedback_works_as_collector(self):
         """
         Member is allowed to give feedback when he is assiged to the Pickup.
-
-        Test:
-        1. log user in as group member
-        2. user gives feedback to pickup
-        3. feedback created
         """
         self.client.force_login(user=self.collector)
         response = self.client.post(self.url, self.feedback_post, format='json')
@@ -133,7 +110,6 @@ class FeedbackTest(APITestCase):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_200_OK, response.data)
         self.assertEqual(len(response.data), 0)
-        # Why is this list empty?
 
     def test_list_feedback_works_as_group_member(self):
         """
@@ -151,7 +127,6 @@ class FeedbackTest(APITestCase):
         self.client.force_login(user=self.collector)
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_200_OK, response.data)
-        print(response.data)
         self.assertEqual(len(response.data), 2)
 
     def test_retrieve_feedback_fails_as_non_user(self):
@@ -184,3 +159,11 @@ class FeedbackTest(APITestCase):
         self.client.force_login(user=self.collector)
         response = self.client.get(self.feedback_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK, response.data)
+
+    def test_create_future_feedback_fails_as_collector(self):
+        """
+        Collector is NOT allowed to leave feedback for future pickup
+        """
+        self.client.force_login(user=self.collector)
+        response = self.client.post(self.url, self.future_feedback_post, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST, response.data)
