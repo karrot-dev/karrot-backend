e01a2b727c8f937c5f5012e0bb3538822b2c823a
Lukas Gebhard <lukas.gebhard@tf.uni-freiburg.de>
2018-02-28 20:04:22 +0100

    Refine and add tests for password reset

--- foodsaving/userauth/tests/test_passwordreset_api.py
+++ foodsaving/userauth/tests/test_passwordreset_api.py
@@ -1,47 +1,12 @@
+from datetime import timedelta
+
 from django.core import mail
+from django.utils import timezone
 from rest_framework import status
 from rest_framework.test import APITestCase
 
 from foodsaving.users.factories import VerifiedUserFactory
 from foodsaving.userauth.models import VerificationCode
-
-
-class TestRequestPasswordReset(APITestCase):
-    def setUp(self):
-        self.verified_user = VerifiedUserFactory(email='reset_test@example.com')
-        self.password = 'forgotten'
-        self.url = '/api/auth/reset_password/request/'
-        mail.outbox = []
-
-    def test_request_reset_password_succeeds(self):
-        response = self.client.post(self.url, {'email': self.verified_user.email})
-        self.assertEqual(response.data, {})
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        self.assertEqual(len(mail.outbox), 1)
-        self.assertEqual(mail.outbox[0].subject, 'Forgot your password?')
-        self.assertEqual(mail.outbox[0].to, [self.verified_user.email])
-
-    def test_request_reset_password_fails_if_wrong_mail(self):
-        response = self.client.post(self.url, {'email': 'wrong@example.com'})
-        self.assertEqual(response.data, {'email': ['Unknown e-mail address']})
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(len(mail.outbox), 0)
-
-    def test_request_reset_password_fails_if_no_email(self):
-        response = self.client.post(self.url)
-        self.assertEqual(response.data, {'email': ['This field is required.']})
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(len(mail.outbox), 0)
-
-    def test_request_reset_password_with_similar_email_succeeds(self):
-        response = self.client.post(self.url, {'email': 'RESET_test@example.com'})
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        self.assertEqual(len(mail.outbox), 1)
-        self.assertEqual(mail.outbox[0].to, [self.verified_user.email])
-
-    def test_request_reset_password_disables_login(self):
-        self.client.post(self.url, {'email': 'RESET_test@example.com'})
-        self.assertFalse(self.client.login(email=self.verified_user.email, password=self.password))
 
 
 class TestPasswordReset(APITestCase):
@@ -55,7 +20,49 @@ class TestPasswordReset(APITestCase):
         self.verified_user.set_password(self.old_password)
         mail.outbox = []
 
-    def test_request_reset_password_succeeds(self):
+    def test_request_password_reset_succeeds(self):
+        response = self.client.post(self.url_request_password_reset, {'email': self.verified_user.email})
+        self.assertEqual(response.data, {})
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].subject, 'Forgot your password?')
+        self.assertEqual(mail.outbox[0].to, [self.verified_user.email])
+
+    def test_request_password_reset_fails_if_wrong_mail(self):
+        response = self.client.post(self.url_request_password_reset, {'email': 'wrong@example.com'})
+        self.assertEqual(response.data, {'email': ['Unknown e-mail address']})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(len(mail.outbox), 0)
+
+    def test_request_password_reset_fails_if_no_email(self):
+        response = self.client.post(self.url_request_password_reset)
+        self.assertEqual(response.data, {'email': ['This field is required.']})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(len(mail.outbox), 0)
+
+    def test_request_password_reset_with_similar_email_succeeds(self):
+        response = self.client.post(self.url_request_password_reset, {'email': 'RESET_test@example.com'})
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].to, [self.verified_user.email])
+
+    def test_request_password_reset_disables_login(self):
+        self.client.post(self.url_request_password_reset, {'email': self.verified_user.email})
+        self.assertFalse(self.client.login(email=self.verified_user.email, password=self.old_password))
+
+    def test_request_password_reset_twice_succeeds(self):
+        self.client.post(self.url_request_password_reset, {'email': self.verified_user.email})
+        response = self.client.post(self.url_request_password_reset, {'email': self.verified_user.email})
+        self.assertEqual(response.data, {})
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(len(mail.outbox), 2)
+
+        code = VerificationCode.objects.get(user=self.verified_user, type=self.type).code
+        response = self.client.post(self.url_password_reset, {'code': code, 'new_password': self.new_password})
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(len(mail.outbox), 3)
+
+    def test_password_reset_succeeds(self):
         self.client.post(self.url_request_password_reset, {'email': self.verified_user.email})
         code = VerificationCode.objects.get(user=self.verified_user, type=self.type).code
         response = self.client.post(self.url_password_reset, {'code': code, 'new_password': self.new_password})
@@ -68,3 +75,55 @@ class TestPasswordReset(APITestCase):
         self.assertFalse(self.client.login(email=self.verified_user.email, password=self.old_password))
         # Login with the new password works
         self.assertTrue(self.client.login(email=self.verified_user.email, password=self.new_password))
+
+    def test_password_reset_fails_without_verification_code(self):
+        self.client.post(self.url_request_password_reset, {'email': self.verified_user.email})
+        response = self.client.post(self.url_password_reset, {'new_password': self.new_password})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data, {'code': ['This field is required.']})
+        self.assertEqual(len(mail.outbox), 1)
+
+        # Login with the old password does not work
+        self.assertFalse(self.client.login(email=self.verified_user.email, password=self.old_password))
+        # Login with the new password does not work either
+        self.assertFalse(self.client.login(email=self.verified_user.email, password=self.new_password))
+
+    def test_password_reset_fails_if_verification_code_too_old(self):
+        self.client.post(self.url_request_password_reset, {'email': self.verified_user.email})
+        verification_code = VerificationCode.objects.get(user=self.verified_user, type=self.type)
+        verification_code.created_at = timezone.now() - timedelta(days=8)
+        verification_code.save()
+        response = self.client.post(self.url_password_reset, {'code': verification_code.code,
+                                                              'new_password': self.new_password})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data['code'], ['Verification code has expired'])
+        self.assertEqual(len(mail.outbox), 1)
+
+        # Login with the old password does not work
+        self.assertFalse(self.client.login(email=self.verified_user.email, password=self.old_password))
+        # Login with the new password does not work either
+        self.assertFalse(self.client.login(email=self.verified_user.email, password=self.new_password))
+
+    def test_password_reset_fails_without_new_password(self):
+        self.client.post(self.url_request_password_reset, {'email': self.verified_user.email})
+        code = VerificationCode.objects.get(user=self.verified_user, type=self.type).code
+        response = self.client.post(self.url_password_reset, {'code': code})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data, {'new_password': ['This field is required.']})
+        self.assertEqual(len(mail.outbox), 1)
+
+        # Login with the old password does not work
+        self.assertFalse(self.client.login(email=self.verified_user.email, password=self.old_password))
+
+    def test_password_reset_fails_with_previous_verification_code(self):
+        self.client.post(self.url_request_password_reset, {'email': self.verified_user.email})
+        code = VerificationCode.objects.get(user=self.verified_user, type=self.type).code
+        self.client.post(self.url_request_password_reset, {'email': self.verified_user.email})
+        response = self.client.post(self.url_password_reset, {'code': code, 'new_password': self.new_password})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data['code'], ['Verification code is invalid'])
+
+        # Login with the old password does not work
+        self.assertFalse(self.client.login(email=self.verified_user.email, password=self.old_password))
+        # Login with the new password does not work either
+        self.assertFalse(self.client.login(email=self.verified_user.email, password=self.new_password))
