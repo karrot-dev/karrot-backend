51451b7900a2b752cbd12d8bf1de69abc537aca6
Nick Sellen <git@nicksellen.co.uk>
2018-03-03 12:38:31 +0100

    Summary emails ignore some users, add task
    ... also record stats for group periodic tasks and summary email

--- foodsaving/webhooks/models.py
+++ foodsaving/webhooks/models.py
@@ -1,7 +1,20 @@
+from dateutil.relativedelta import relativedelta
+from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.postgres.fields import JSONField
 from django.db.models import CharField, TextField, BigAutoField
+from django.utils import timezone
 
+from config import settings
 from foodsaving.base.base_models import BaseModel
+
+
+class EmailEventManager(BaseUserManager):
+
+    def ignored_addresses(self):
+        return self.filter(
+            created_at__gte=timezone.now() - relativedelta(months=6),
+            event__in=settings.EMAIL_EVENTS_AVOID
+        ).values('address')
 
 
 class EmailEvent(BaseModel):
@@ -9,3 +22,4 @@ class EmailEvent(BaseModel):
     address = TextField()
     event = CharField(max_length=255)
     payload = JSONField()
+    objects = EmailEventManager()
