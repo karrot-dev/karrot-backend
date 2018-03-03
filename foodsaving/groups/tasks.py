373b7d5fff08acb0113180866a38cbef046b6c46
John Bryden <john@jbryden.co.uk>
2018-03-03 16:28:05 +0100

    Fixed tests and migrations

--- foodsaving/groups/tasks.py
+++ foodsaving/groups/tasks.py
@@ -25,13 +25,11 @@ def record_stats():
 
 
 def send_inactive_in_group_notification_to_user(user, group):
-    print('Sending email to userid=', user.id, 'regarding inactivity in groupid=', group.id)
     email = prepare_user_inactive_in_group_email(user, group)
     email.send()
 
 
 def send_removal_from_group_notification_to_user(user, group):
-    print('Sending email to userid=', user.id, 'regarding removal from groupid=', group.id)
     email = prepare_user_removed_from_group_email(user, group)
     email.send()
 
@@ -40,20 +38,19 @@ def send_removal_from_group_notification_to_user(user, group):
 def process_inactive_users():
     now = timezone.now()
 
-    print("Processing inactive users")
+    count_users_flagged_inactive = 0
+    count_users_removed = 0
 
     remove_threshold_date = now - timedelta(days=settings.NUMBER_OF_DAYS_UNTIL_REMOVED_FROM_GROUP)
-    print("Removing inactive users in groups who have been inactive since:",
-          datetime.strftime(remove_threshold_date, '%Y-%m-%d'))
     for gm in GroupMembership.objects.all().filter(lastseen_at__lte=remove_threshold_date, active=False):
         send_removal_from_group_notification_to_user(gm.user, gm.group)
         gm.delete()
+        count_users_removed += 1
 
     inactive_threshold_date = now - timedelta(days=settings.NUMBER_OF_DAYS_UNTIL_INACTIVE_IN_GROUP)
-    print("Flagging inactive users in groups who have been inactive since:",
-          datetime.strftime(inactive_threshold_date, '%Y-%m-%d'))
     for gm in GroupMembership.objects.all().filter(lastseen_at__lte=inactive_threshold_date, active=True):
         send_inactive_in_group_notification_to_user(gm.user, gm.group)
         gm.active = False
         gm.save()
+        count_users_flagged_inactive += 1
 
--- foodsaving/groups/tests/test_tasks.py
+++ foodsaving/groups/tests/test_tasks.py
@@ -13,31 +13,41 @@ from datetime import timedelta
 
 class TestProcessInactiveUsers (TestCase):
     def setUp(self):
-        self.member1 = UserFactory()
-        self.member2 = UserFactory()
-        self.group = GroupFactory(members=[self.member1, self.member2])
+        self.activeUser = UserFactory()
+        self.inactiveUser = UserFactory()
+        self.deletedUser = UserFactory()
+        self.group = GroupFactory(members=[self.activeUser, self.inactiveUser, self.deletedUser])
 
         now = timezone.now()
 
         inactive_email_date = now - timedelta(days=settings.NUMBER_OF_DAYS_UNTIL_INACTIVE_IN_GROUP)
-        self.membership1 = GroupMembership.objects.get(group=self.group, user=self.member1)
+        self.membership1 = GroupMembership.objects.get(group=self.group, user=self.inactiveUser)
         self.membership1.lastseen_at = inactive_email_date
         self.membership1.save()
 
         remove_email_date = now - timedelta(days=settings.NUMBER_OF_DAYS_UNTIL_REMOVED_FROM_GROUP + 1)
-        self.membership2 = GroupMembership.objects.get(group=self.group, user=self.member2)
+        self.membership2 = GroupMembership.objects.get(group=self.group, user=self.deletedUser)
         self.membership2.lastseen_at = remove_email_date
         self.membership2.active = False
         self.membership2.save()
 
         mail.outbox = []
 
-    def test_process_user_inactive(self):
+    def test_process_inactive_users_removes_one_user(self):
         process_inactive_users()
-        self.assertEqual(len(mail.outbox), 2)
+        self.assertEqual(len(GroupMembership.objects.all()), 2)
+
+    def test_process_inactive_users_leaves_active_user_alone(self):
+        process_inactive_users()
+        activeMembership = GroupMembership.objects.get(group=self.group, user=self.activeUser)
+        self.assertEqual(activeMembership.active, True)
+
+    def test_process_inactive_users_flags_inactive_user(self):
+        process_inactive_users()
         self.membership1.refresh_from_db()
         self.assertEqual(self.membership1.active, False)
-        self.assertEqual(len(GroupMembership.objects.all()), 1)
-        self.assertEqual(GroupMembership.objects.first(), self.member1)
 
+    def test_process_inactive_users_sends_emails(self):
+        process_inactive_users()
+        self.assertEqual(len(mail.outbox), 2)
 
