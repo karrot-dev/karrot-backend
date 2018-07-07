020b487f58455d20713abd93c1c84e4ff11f440e
Tilmann Becker <tilmann.becker+github@gmail.com>
2018-07-07 12:26:34 +0200

    Add new store statistics endpoint (#560)
    * add store statistics endpoint
    
    * add test

--- foodsaving/stores/api.py
+++ foodsaving/stores/api.py
@@ -1,9 +1,13 @@
+from django.db.models import Avg, Count, Q, Sum
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import mixins
+from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
+from foodsaving.pickups.models import PickupDate
 from foodsaving.stores.models import Store as StoreModel
 from foodsaving.stores.serializers import StoreSerializer
 from foodsaving.utils.mixins import PartialUpdateModelMixin
@@ -31,4 +35,23 @@ class StoreViewSet(
     permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
-        return self.queryset.filter(group__members=self.request.user)
+        qs = self.queryset.filter(group__members=self.request.user)
+        if self.action == 'statistics':
+            return qs.annotate(
+                feedback_count=Count('pickup_dates__feedback', distinct=True),
+                pickups_done=Count('pickup_dates', filter=Q(pickup_dates__in=PickupDate.objects.done()), distinct=True)
+            )
+        else:
+            return qs
+
+    @action(detail=True)
+    def statistics(self, request, pk=None):
+        instance = self.get_object()
+        weight = instance.pickup_dates.annotate(avg_weight=Avg('feedback__weight'))\
+            .aggregate(estimated_weight=Sum('avg_weight'))['estimated_weight']
+        data = {
+            'feedback_count': instance.feedback_count,
+            'feedback_weight': round(weight or 0),
+            'pickups_done': instance.pickups_done,
+        }
+        return Response(data)
