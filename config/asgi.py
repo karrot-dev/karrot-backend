634df5aa3a49960925b99ea233184e961ca2aa91
Nick Sellen <git@nicksellen.co.uk>
2018-07-22 11:15:08 +0200

    Upgrade entrypoint to provide an asgi application

--- config/asgi.py
+++ config/asgi.py
@@ -1,7 +1,12 @@
-import os
+"""
+ASGI entrypoint. Configures Django and then runs the application
+defined in the ASGI_APPLICATION setting.
+"""
 
-from channels.asgi import get_channel_layer
+import os
+import django
+from channels.routing import get_default_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-
-channel_layer = get_channel_layer()
+django.setup()
+application = get_default_application()
