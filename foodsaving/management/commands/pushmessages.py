11c04e9fc2bcd5e6732814372d50d20cdb525bed
Tilmann Becker <tilmann.becker@gmail.com>
2018-08-05 18:43:09 +0200

    add pushmessages command

---
+++ foodsaving/management/commands/pushmessages.py
@@ -0,0 +1,10 @@
+import subprocess
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        push_cmd = 'tx push -s'
+
+        print(push_cmd)
+        subprocess.run(push_cmd, shell=True)
