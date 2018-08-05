b64c547c80b4397634ddaebfa7ca2398d3f7c442
Tilmann Becker <tilmann.becker@gmail.com>
2018-08-05 18:51:18 +0200

    reformat command

--- foodsaving/management/commands/makemessages.py
+++ foodsaving/management/commands/makemessages.py
@@ -5,7 +5,13 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     def handle(self, *args, **options):
         extract_cmd = 'pybabel extract -F babel.cfg -o foodsaving/locale/django.pot .'
-        update_cmd = 'pybabel update -D django -l en -i foodsaving/locale/django.pot -o foodsaving/locale/en/LC_MESSAGES/django.po'
+        update_cmd = ' '.join([
+            'pybabel update',
+            '-D django',
+            '-l en',
+            '-i foodsaving/locale/django.pot',
+            '-o foodsaving/locale/en/LC_MESSAGES/django.po',
+        ])
 
         print(extract_cmd)
         subprocess.run(extract_cmd, shell=True)
