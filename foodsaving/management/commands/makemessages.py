30ae73baa54cbc65aef2a6716a424b4b2ae712a1
Tilmann Becker <tilmann.becker@gmail.com>
2018-08-05 19:38:14 +0200

    use pot as po

--- foodsaving/management/commands/makemessages.py
+++ foodsaving/management/commands/makemessages.py
@@ -4,19 +4,11 @@ from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        extract_cmd = 'pybabel extract -F babel.cfg -o foodsaving/locale/django.pot .'
-        update_cmd = ' '.join([
-            'pybabel update',
-            '-D django',
-            '-l en',
-            '-i foodsaving/locale/django.pot',
+        extract_cmd = ' '.join([
+            'pybabel extract',
+            '-F babel.cfg',
             '-o foodsaving/locale/en/LC_MESSAGES/django.po',
+            '.',
         ])
-
         print(extract_cmd)
         subprocess.run(extract_cmd, shell=True)
-
-        print()
-        print()
-        print(update_cmd)
-        subprocess.run(update_cmd, shell=True)
