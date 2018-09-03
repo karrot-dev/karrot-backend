074c83285bba8a8805bf35dec9893771220b1715
Tilmann Becker <tilmann.becker@gmail.com>
2018-09-03 23:53:08 +0200

    Use slightly better approach to count users without groups

--- foodsaving/users/stats.py
+++ foodsaving/users/stats.py
@@ -1,5 +1,4 @@
 from django.contrib.auth import get_user_model
-from django.db.models import Count
 
 from foodsaving.groups.models import GroupMembership
 from foodsaving.webhooks.models import EmailEvent
@@ -13,26 +12,16 @@ def get_users_stats():
     active_users_count = active_users.count()
 
     fields = {
-        'active_count':
-        active_users_count,
-        'active_unverified_count':
-        active_users.filter(mail_verified=False).count(),
-        'active_ignored_email_count':
-        active_users.filter(email__in=EmailEvent.objects.ignored_addresses()).count(),
-        'active_with_location_count':
-        active_users.exclude(latitude=None).exclude(longitude=None).count(),
-        'active_with_mobile_number_count':
-        active_users.exclude(mobile_number='').count(),
-        'active_with_description_count':
-        active_users.exclude(description='').count(),
-        'active_with_photo_count':
-        active_users.exclude(photo='').count(),
-        'active_memberships_per_active_user_avg':
-        active_membership_count / active_users_count,
-        'no_membership_count':
-        User.objects.annotate(groups_count=Count('groupmembership')).filter(groups_count=0, deleted=False).count(),
-        'deleted_count':
-        User.objects.filter(deleted=True).count(),
+        'active_count': active_users_count,
+        'active_unverified_count': active_users.filter(mail_verified=False).count(),
+        'active_ignored_email_count': active_users.filter(email__in=EmailEvent.objects.ignored_addresses()).count(),
+        'active_with_location_count': active_users.exclude(latitude=None).exclude(longitude=None).count(),
+        'active_with_mobile_number_count': active_users.exclude(mobile_number='').count(),
+        'active_with_description_count': active_users.exclude(description='').count(),
+        'active_with_photo_count': active_users.exclude(photo='').count(),
+        'active_memberships_per_active_user_avg': active_membership_count / active_users_count,
+        'no_membership_count': User.objects.filter(groupmembership=None, deleted=False).count(),
+        'deleted_count': User.objects.filter(deleted=True).count(),
     }
 
     return fields
