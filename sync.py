c7b9578e5dfa24885b03d62ba589c05ea17abbf1
Tilmann Becker <tilmann.becker@gmail.com>
2018-10-16 11:00:03 +0200

    make pip overwrite cached source directories without prompting

--- sync.py
+++ sync.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
 
 import subprocess
-subprocess.run(['pip-sync', 'requirements.txt', 'requirements-dev.txt'])
+import os
+environ = os.environ.copy()
+
+# do not prompt if dependency source already exists, just wipe and get the required version
+environ['PIP_EXISTS_ACTION'] = 'w'
+
+subprocess.run(['pip-sync', 'requirements.txt', 'requirements-dev.txt'], env=environ)
