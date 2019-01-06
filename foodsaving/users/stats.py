937812c38c10163453d8bfaa5c5c6151ae3a2faa
Nick Sellen <git@nicksellen.co.uk>
2019-01-06 15:28:01 +0100

    Add stats for how many users have done a pickup

--- foodsaving/users/stats.py
+++ foodsaving/users/stats.py
@@ -1,6 +1,9 @@
+from dateutil.relativedelta import relativedelta
 from django.contrib.auth import get_user_model
+from django.utils import timezone
 
 from foodsaving.groups.models import GroupMembership
+from foodsaving.pickups.models import PickupDate
 from foodsaving.webhooks.models import EmailEvent
 
 
@@ -10,6 +13,10 @@ def get_users_stats():
     active_users = User.objects.filter(groupmembership__in=GroupMembership.objects.active(), deleted=False).distinct()
     active_membership_count = GroupMembership.objects.active().count()
     active_users_count = active_users.count()
+
+    done_pickup_filter = {'pickup_dates__in':PickupDate.objects.exclude_disabled().filter(date__lt=timezone.now())}
+    pickup_active_users = active_users.filter(**done_pickup_filter)
+    pickup_users = User.objects.filter(**done_pickup_filter)
 
     fields = {
         'active_count': active_users_count,
@@ -22,6 +29,8 @@ def get_users_stats():
         'active_memberships_per_active_user_avg': active_membership_count / active_users_count,
         'no_membership_count': User.objects.filter(groupmembership=None, deleted=False).count(),
         'deleted_count': User.objects.filter(deleted=True).count(),
+        'pickup_active_count': pickup_active_users.count(),
+        'pickup_count': pickup_users.count(),
     }
 
     return fields
--- foodsaving/users/tests/test_stats.py
+++ foodsaving/users/tests/test_stats.py
@@ -1,8 +1,11 @@
+from dateutil.relativedelta import relativedelta
 from django.test import TestCase
 from django.utils import timezone
 
 from foodsaving.groups.factories import GroupFactory
 from foodsaving.groups.models import GroupMembership
+from foodsaving.pickups.factories import PickupDateFactory
+from foodsaving.stores.factories import StoreFactory
 from foodsaving.users import stats
 from foodsaving.users.factories import UserFactory, VerifiedUserFactory
 from foodsaving.webhooks.models import EmailEvent
@@ -12,9 +15,18 @@ class TestUserStats(TestCase):
     def test_user_stats(self):
         self.maxDiff = None
 
+        def do_pickup(store, user, **kwargs):
+            pickup = PickupDateFactory(store=store, date=timezone.now() - relativedelta(**kwargs))
+            pickup.add_collector(user)
+
         # 9 verified users, 1 unverified user
         users = [VerifiedUserFactory() for _ in range(9)]
         users.insert(0, UserFactory())
+
+        # 5 fully inactive users (only in one group and marked as inactive in that group)
+        inactive_users = [VerifiedUserFactory() for _ in range(5)]
+        inactive_group = GroupFactory(members=inactive_users)
+        GroupMembership.objects.filter(group=inactive_group).update(inactive_at=timezone.now())
 
         # 1 deleted user
         deleted_user = UserFactory()
@@ -50,10 +62,19 @@ class TestUserStats(TestCase):
         photo_user.save()
 
         # 2 groups where everybody is active, 1 where everybody is inactive
-        GroupFactory(members=users[:9])
+        group = GroupFactory(members=users[:9])
         GroupFactory(members=users[:9])
         group_all_inactive = GroupFactory(members=users[:9])
         GroupMembership.objects.filter(group=group_all_inactive).update(inactive_at=timezone.now())
+
+        # 1 active user that did a pickup
+        store = StoreFactory(group=group)
+        do_pickup(store, users[0], days=10)
+
+        # 2 inactive user that did a pickup long ago
+        inactive_store = StoreFactory(group=inactive_group)
+        do_pickup(inactive_store, inactive_users[0], days=60)
+        do_pickup(inactive_store, inactive_users[1], days=90)
 
         points = stats.get_users_stats()
 
@@ -69,5 +90,7 @@ class TestUserStats(TestCase):
                 'active_memberships_per_active_user_avg': 2.0,
                 'no_membership_count': 1,
                 'deleted_count': 1,
+                'pickup_active_count': 1,
+                'pickup_count': 3,
             }
         )
