b63fc7e248e44ec94d20ce8aacc06d73aa287c87
Tilmann Becker <tilmann.becker@gmail.com>
2019-01-06 13:11:04 +0100

    add stats about invitations

---
+++ foodsaving/invitations/stats.py
@@ -0,0 +1,23 @@
+from influxdb_metrics.loader import write_points
+
+from foodsaving.groups.stats import group_tags
+
+
+def invitation_created(invitation):
+    write_points([{
+        'measurement': 'karrot.events',
+        'tags': group_tags(invitation.group),
+        'fields': {
+            'invitation_created': 1
+        },
+    }])
+
+
+def invitation_accepted(invitation):
+    write_points([{
+        'measurement': 'karrot.events',
+        'tags': group_tags(invitation.group),
+        'fields': {
+            'invitation_accepted': 1
+        },
+    }])
