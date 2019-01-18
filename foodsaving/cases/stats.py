d91c9369d4ea3cadd428416e1cc3785f31791504
Tilmann Becker <tilmann.becker@gmail.com>
2019-01-18 18:49:14 +0100

    add case type to stats

--- foodsaving/cases/stats.py
+++ foodsaving/cases/stats.py
@@ -3,10 +3,18 @@ from influxdb_metrics.loader import write_points
 from foodsaving.groups.stats import group_tags
 
 
+def case_tags(case):
+    tags = group_tags(case.group)
+    tags.update({
+        'type': case.type,
+    })
+    return tags
+
+
 def case_created(case):
     write_points([{
         'measurement': 'karrot.events',
-        'tags': group_tags(case.group),
+        'tags': case_tags(case),
         'fields': {
             'case_created': 1
         },
@@ -16,7 +24,7 @@ def case_created(case):
 def voted(case):
     write_points([{
         'measurement': 'karrot.events',
-        'tags': group_tags(case.group),
+        'tags': case_tags(case),
         'fields': {
             'case_vote': 1
         },
@@ -26,7 +34,7 @@ def voted(case):
 def vote_changed(case):
     write_points([{
         'measurement': 'karrot.events',
-        'tags': group_tags(case.group),
+        'tags': case_tags(case),
         'fields': {
             'case_vote_changed': 1
         },
@@ -36,7 +44,7 @@ def vote_changed(case):
 def vote_deleted(case):
     write_points([{
         'measurement': 'karrot.events',
-        'tags': group_tags(case.group),
+        'tags': case_tags(case),
         'fields': {
             'case_vote_deleted': 1
         },
@@ -46,8 +54,9 @@ def vote_deleted(case):
 def get_case_stats(group):
     fields = {
         'count_total': group.cases.count(),
-        'count_ongoing': group.cases.filter(is_decided=False).count(),
-        'count_decided': group.cases.filter(is_decided=True).count(),
+        'count_ongoing': group.cases.ongoing().count(),
+        'count_decided': group.cases.decided().count(),
+        'count_cancelled': group.cases.cancelled().count(),
     }
 
     return [{
