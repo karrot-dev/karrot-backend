04b64bed6489807a1f417bb13994dd9302f224cc
Tilmann Becker <tilmann.becker@gmail.com>
2019-01-20 18:45:04 +0100

    reword emails

--- foodsaving/cases/tests/test_api.py
+++ foodsaving/cases/tests/test_api.py
@@ -78,8 +78,8 @@ class TestConflictResolutionAPI(APITestCase, ExtractPaginationMixin):
         self.assertEqual(len(mail.outbox), 2)
         email_to_affected_user = next(email for email in mail.outbox if email.to[0] == self.affected_member.email)
         email_to_editor = next(email for email in mail.outbox if email.to[0] == notification_member.email)
-        self.assertIn('with you', email_to_affected_user.subject)
-        self.assertIn('with {}'.format(self.affected_member.display_name), email_to_editor.subject)
+        self.assertIn('about you', email_to_affected_user.subject)
+        self.assertIn('about {}'.format(self.affected_member.display_name), email_to_editor.subject)
 
         # vote on option
         response = self.client.post(
