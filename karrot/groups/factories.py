db1345317cdcd5f482af847893675652ca313239
Stefan Thelin <stefan.josefsson@gmail.com>
2019-03-09 19:23:09 +0100

    Added welcome_message to Group model and application_accepted email (#680)
    * Added welcome_message to Group model and application_accepted email
    
    * Remove translation tags from application_accepted template
    
    * PEP8 fix in factories.py
    
    * Wrap group.welcome_message in if tags in email template

--- karrot/groups/factories.py
+++ karrot/groups/factories.py
@@ -25,6 +25,7 @@ class GroupFactory(DjangoModelFactory):
     description = LazyAttribute(lambda x: faker.sentence(nb_words=40))
     public_description = LazyAttribute(lambda x: faker.sentence(nb_words=20))
     application_questions = LazyAttribute(lambda x: faker.sentence(nb_words=20))
+    welcome_message = LazyAttribute(lambda x: faker.sentence(nb_words=30))
 
 
 class PlaygroundGroupFactory(GroupFactory):
