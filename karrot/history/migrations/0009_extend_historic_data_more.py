0b8ca8c7015ddd567abdd08e508a27c6ee3ed778
Tilmann Becker <tilmann.becker@gmail.com>
2019-10-19 13:05:16 +0200

    fix history pickup payload

---
+++ karrot/history/migrations/0009_extend_historic_data_more.py
@@ -0,0 +1,25 @@
+from django.db import migrations
+
+BATCH_SIZE = 1000
+
+
+def migrate(apps, schema_editor):
+    History = apps.get_model('history', 'History')
+
+    # rewrite pickup date payload date field from string to list
+    save_payload = []
+
+    for h in History.objects.filter(payload__date__0__isnull=True, payload__date__isnull=False):
+        h.payload['date'] = [h.payload['date']]
+        save_payload.append(h)
+
+    History.objects.bulk_update(save_payload, fields=['payload'], batch_size=BATCH_SIZE)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('history', '0008_extend_historic_data'),
+    ]
+
+    operations = [migrations.RunPython(migrate, migrations.RunPython.noop, elidable=True)]
