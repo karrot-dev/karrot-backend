9dd23fd3d6fbe362070de994ef796ce4c4193be7
Nick Sellen <git@nicksellen.co.uk>
2019-11-10 17:27:19 +0100

    Add initial offers API tests

---
+++ karrot/offers/factories.py
@@ -0,0 +1,26 @@
+from django.core.files.uploadedfile import SimpleUploadedFile
+from factory import DjangoModelFactory, SubFactory, LazyAttribute, post_generation
+
+from karrot.groups.factories import GroupFactory
+from karrot.offers.models import Offer
+from karrot.users.factories import UserFactory
+from karrot.utils.tests.fake import faker
+
+
+class OfferFactory(DjangoModelFactory):
+    class Meta:
+        model = Offer
+
+    group = SubFactory(GroupFactory)
+    user = SubFactory(UserFactory)
+    name = LazyAttribute(lambda x: faker.sentence(nb_words=4))
+    description = LazyAttribute(lambda x: faker.name())
+    status = 'active'
+
+    @post_generation
+    def images(self, created, extracted, **kwargs):
+        if created and extracted:
+            for index, image_path in enumerate(extracted):
+                with open(image_path, 'rb') as file:
+                    upload = SimpleUploadedFile(name='foo.jpg', content=file.read(), content_type='image/jpeg')
+                    self.images.create(image=upload, position=index)
---
+++ karrot/offers/tests/test_api.py
@@ -0,0 +1,80 @@
+import json
+import os
+from io import StringIO
+
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from karrot.groups.factories import GroupFactory
+from karrot.offers.factories import OfferFactory
+from karrot.users.factories import UserFactory
+from karrot.utils.tests.fake import faker
+
+image_path = os.path.join(os.path.dirname(__file__), './photo.jpg')
+
+
+def encode_offer_data(data):
+    post_data = {}
+    for index, image in enumerate(data.get('images', [])):
+        image_file = image.pop('image')
+        post_data['images.{}.image'.format(index)] = image_file
+    data_file = StringIO(json.dumps(data))
+    setattr(data_file, 'content_type', 'application/json')
+    post_data['document'] = data_file
+    return post_data
+
+
+class TestOffersAPI(APITestCase):
+    def setUp(self):
+        self.user = UserFactory()
+        self.group = GroupFactory(members=[self.user])
+
+    def test_fetch_offer(self):
+        self.client.force_login(user=self.user)
+        offer = OfferFactory(user=self.user, group=self.group)
+        response = self.client.get('/api/offers/{}/'.format(offer.id))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['name'], offer.name)
+
+    def test_create_offer(self):
+        self.client.force_login(user=self.user)
+        with open(image_path, 'rb') as image_file:
+            data = {
+                'name': faker.name(),
+                'description': faker.text(),
+                'group': self.group.id,
+                'images': [{
+                    'position': 0,
+                    'image': image_file
+                }],
+            }
+            response = self.client.post('/api/offers/', data=encode_offer_data(data))
+            self.assertEqual(response.status_code, status.HTTP_201_CREATED, response.data)
+            self.assertEqual(response.data['name'], data['name'])
+            self.assertTrue('full_size' in response.data['images'][0]['image_urls'])
+
+    def test_update_offer(self):
+        offer = OfferFactory(user=self.user, group=self.group, images=[image_path])
+        self.client.force_login(user=self.user)
+        data = {
+            'name': 'woo',
+        }
+        response = self.client.patch('/api/offers/{}/'.format(offer.id), data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.data)
+        self.assertEqual(response.data['name'], data['name'])
+
+    def test_add_image(self):
+        offer = OfferFactory(user=self.user, group=self.group, images=[image_path])
+        self.client.force_login(user=self.user)
+        with open(image_path, 'rb') as image_file:
+            data = {
+                'images': [{
+                    'position': 1,
+                    'image': image_file
+                }],
+            }
+            response = self.client.patch(
+                '/api/offers/{}/'.format(offer.id), encode_offer_data(data), format='multipart'
+            )
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+            self.assertEqual(len(response.data['images']), 2)
