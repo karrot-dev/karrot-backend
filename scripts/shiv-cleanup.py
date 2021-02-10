5e4d350093ecba2ecdd09bf54de36d02bdabd604
Nick Sellen <git@nicksellen.co.uk>
2021-02-10 21:11:48 +0000

    Don't use python 3.9 features

--- scripts/shiv-cleanup.py
+++ scripts/shiv-cleanup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+import re
 import shutil
 
 # see https://shiv.readthedocs.io/en/latest/#preamble
@@ -16,6 +17,6 @@ name, build_id = current.name.split('_')
 if __name__ == "__main__":
     for path in cache_path.iterdir():
         # "." prefix and "_lock" suffix are present on lock files which we also want to remove
-        test_path = path.name.removeprefix('.').removesuffix('_lock')
+        test_path = re.sub('_lock$', '', re.sub(r'^\.', '', path.name))
         if test_path.startswith(f"{name}_") and not test_path.endswith(build_id):
             shutil.rmtree(path) if os.path.isdir(path) else os.remove(path)
