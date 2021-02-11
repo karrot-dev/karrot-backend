dcca214f8a00ea53f05d18378a60f2cd1e9b8ba4
Nick Sellen <git@nicksellen.co.uk>
2021-02-11 11:32:04 +0000

    Fixup shiv cleanup script
    Only cleans up files if they are not in use

--- scripts/shiv-cleanup.py
+++ scripts/shiv-cleanup.py
@@ -19,17 +19,14 @@ name, build_id = current.name.split('_')
 def get_mapped_files():
     files = set()
 
-    def check_pid(pid):
+    for pid in [p.pid for p in psutil.process_iter()]:
         try:
             # quite linux specific... lots of assumptions...
             mapped_file = os.listdir(f"/proc/{pid}/map_files")
             for mapped_file_name in mapped_file:
-                files.append(os.readlink(f"/proc/{pid}/map_files/{mapped_file_name}"))
+                files.add(os.readlink(f"/proc/{pid}/map_files/{mapped_file_name}"))
         except PermissionError:
             pass
-
-    for p in psutil.process_iter():
-        check_pid(p.pid)
 
     return files
 
@@ -37,11 +34,11 @@ def get_mapped_files():
 if __name__ == "__main__":
     mapped_files = get_mapped_files()
     for path in cache_path.iterdir():
-        if path in mapped_files:
-            print('skipping cleanup for', path)
+        absolute_path = str(path.resolve())
+        # if we have any files in use in this dir, skip...
+        if any(mapped_file.startswith(absolute_path) for mapped_file in mapped_files):
             continue
         # "." prefix and "_lock" suffix are present on lock files which we also want to remove
         test_path = re.sub('_lock$', '', re.sub(r'^\.', '', path.name))
         if test_path.startswith(f"{name}_") and not test_path.endswith(build_id):
-            print('shiv cleanup', path)
             shutil.rmtree(path) if os.path.isdir(path) else os.remove(path)
