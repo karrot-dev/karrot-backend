da8ab1ac338367043b72c93f1eda9b3b7180fe87
Dominic Schwarz <brunooo@gmx.de>
2021-03-23 12:27:55 +0100

    fix(activities-api): remove unused rest_framework.status

--- karrot/activities/api.py
+++ karrot/activities/api.py
@@ -8,7 +8,6 @@ from rest_framework.pagination import CursorPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
-from rest_framework import status
 
 from karrot.conversations.api import RetrieveConversationMixin
 from karrot.history.models import History, HistoryTypus
