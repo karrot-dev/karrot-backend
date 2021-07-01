19970162666514737232f2d506e96369159b0aca
Nick Sellen <git@nicksellen.co.uk>
2021-07-01 12:04:06 +0100

    Add default+transltable place names

--- karrot/places/place_types.py
+++ karrot/places/place_types.py
@@ -2,16 +2,17 @@ from django.utils.translation import gettext_lazy as _
 
 # A list of translatable names that groups might use
 translatable_names = {
+    'Unspecified': _('Unspecified'),
     'Store': _('Store'),
-    'Place': _('Place'),
-    # TODO: more?
+    'Sharing point': _('Sharing point'),
+    'Meeting place': _('Meeting place'),
+    'Restaurant': _('Restaurant'),
+    'Market': _('Market'),
 }
 
 # Default types that will be created for new groups
-# (in the future this would be more customizable)
 default_place_types = {
-    'Store': {
-        'icon': 'fas fa-shopping-cart',
+    'Unspecified': {
+        'icon': 'fas fa-map-marker',
     },
-    # TODO: also a generic type?
 }
