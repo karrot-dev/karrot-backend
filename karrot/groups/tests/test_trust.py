e3aaffdb713cbf5c49e8482b09c7a896f0e26a4e
Nick Sellen <git@nicksellen.co.uk>
2021-07-15 10:00:54 +0100

    Test updated list trust endpoint

--- karrot/groups/tests/test_trust.py
+++ karrot/groups/tests/test_trust.py
@@ -1,3 +1,5 @@
+from unittest.mock import ANY
+
 from dateutil.relativedelta import relativedelta
 from django.core import mail
 from django.test import TestCase
@@ -8,6 +10,7 @@ from rest_framework.test import APITestCase
 
 from karrot.groups.factories import GroupFactory
 from karrot.groups.models import GroupMembership, Trust
+from karrot.groups.roles import GROUP_EDITOR
 from karrot.history.models import History, HistoryTypus
 from karrot.users.factories import UserFactory
 
@@ -143,6 +146,23 @@ class TestTrustAPI(APITestCase):
                 membership__group=self.group,
                 membership__user=self.member2,
                 given_by=self.member1,
+                role=GROUP_EDITOR,
+            ).exists()
+        )
+
+    def test_give_trust_for_role(self):
+        self.client.force_login(user=self.member1)
+
+        url = reverse('group-trust-user', args=(self.group.id, self.member2.id))
+        response = self.client.post(url, {'role': 'somerole'})
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(
+            Trust.objects.filter(
+                membership__group=self.group,
+                membership__user=self.member2,
+                given_by=self.member1,
+                role='somerole',
             ).exists()
         )
 
@@ -202,15 +222,45 @@ class TestTrustList(APITestCase):
     def setUp(self):
         self.member1 = UserFactory()
         self.member2 = UserFactory()
-        self.group = GroupFactory(members=[self.member1, self.member2])
+        self.member3 = UserFactory()
+        self.group = GroupFactory(members=[self.member1, self.member2, self.member3])
 
         membership = GroupMembership.objects.get(user=self.member2, group=self.group)
         Trust.objects.create(membership=membership, given_by=self.member1)
         membership = GroupMembership.objects.get(user=self.member1, group=self.group)
         Trust.objects.create(membership=membership, given_by=self.member2)
+        Trust.objects.create(membership=membership, given_by=self.member3, role='anotherrole')
 
     def test_list_trust_for_group(self):
         self.client.force_login(user=self.member1)
         response = self.client.get('/api/groups/{}/'.format(self.group.id))
+        self.assertEqual(
+            response.data['memberships'][self.member1.id]['trust'], [
+                {
+                    'created_at': ANY,
+                    'given_by': self.member2.id,
+                    'role': GROUP_EDITOR
+                },
+                {
+                    'created_at': ANY,
+                    'given_by': self.member3.id,
+                    'role': 'anotherrole'
+                },
+            ]
+        )
+        self.assertEqual(
+            response.data['memberships'][self.member2.id]['trust'], [
+                {
+                    'created_at': ANY,
+                    'given_by': self.member1.id,
+                    'role': GROUP_EDITOR
+                },
+            ]
+        )
+
+    def test_backwards_compatible_trusted_by_field(self):
+        self.client.force_login(user=self.member1)
+        response = self.client.get('/api/groups/{}/'.format(self.group.id))
+        # trust for other roles won't be included in the 'trusted_by' field
         self.assertEqual(response.data['memberships'][self.member1.id]['trusted_by'], [self.member2.id])
         self.assertEqual(response.data['memberships'][self.member2.id]['trusted_by'], [self.member1.id])
