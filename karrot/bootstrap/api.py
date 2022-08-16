78ea0d808e189f811a468651f0e81b1d7413b8c6
Nick Sellen <git@nicksellen.co.uk>
2022-08-16 22:38:30 +0100

    Add more bootstrap data
    Adding "fields" param to specify which fields you want

--- karrot/bootstrap/api.py
+++ karrot/bootstrap/api.py
@@ -1,10 +1,15 @@
 from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.db.models import Q
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from config.options import get_git_rev
+from karrot.activities.models import ActivityType
 from karrot.bootstrap.serializers import BootstrapSerializer, ConfigSerializer
 from karrot.groups.models import Group
+from karrot.places.models import Place
+from karrot.status.helpers import status_data
 from karrot.utils.geoip import geoip_is_available, get_client_ip, ip_to_city
 
 BACKEND_REVISION = get_git_rev()
@@ -38,30 +43,72 @@ class BootstrapViewSet(GenericViewSet):
     serializer_class = BootstrapSerializer  # for OpenAPI generation with drf-spectacular
 
     def list(self, request, *args, **kwargs):
+        fields = request.query_params.get('fields', 'server,config,geoip,user,groups').split(',')
         user = request.user
-        geo_data = None
 
+        data = {
+            'server': {
+                'revision': BACKEND_REVISION,
+            } if 'server' in fields else None,
+            'config': get_config_data() if 'config' in fields else None,
+            'geoip': self.get_geoip(request) if 'geoip' in fields else None,
+            'groups': self.get_groups(user) if 'groups' in fields else None,
+        }
+
+        if user.is_authenticated:
+            data.update({
+                'user': user if 'user' in fields else None,
+                'places': self.get_places(user) if 'places' in fields else None,
+                'users': self.get_users(user) if 'users' in fields else None,
+                'status': self.get_status(user) if 'status' in fields else None,
+                'activity_types': self.get_activity_types(user) if 'activity_types' in fields else None,
+            })
+
+        # only keep fields we want (others should be None anyway...)
+        data = {key: val for key, val in data.items() if key in fields}
+        serializer = BootstrapSerializer(data, context=self.get_serializer_context())
+        return Response(serializer.data)
+
+    @staticmethod
+    def get_geoip(request):
         if geoip_is_available():
             client_ip = get_client_ip(request)
             if client_ip:
                 city = ip_to_city(client_ip)
                 if city:
-                    geo_data = {
+                    return {
                         'lat': city.get('latitude', None),
                         'lng': city.get('longitude', None),
                         'country_code': city.get('country_code', None),
                         'timezone': city.get('time_zone', None),
                     }
 
-        data = {
-            'server': {
-                'revision': BACKEND_REVISION,
-            },
-            'config': get_config_data(),
-            'user': user if user.is_authenticated else None,
-            'geoip': geo_data,
-            'groups': Group.objects.annotate_member_count().annotate_is_user_member(user),
-        }
+    @staticmethod
+    def get_groups(user):
+        return Group.objects.annotate_member_count().annotate_is_user_member(user)
 
-        serializer = BootstrapSerializer(data, context=self.get_serializer_context())
-        return Response(serializer.data)
+    @staticmethod
+    def get_users(user):
+        is_member_of_group = Q(groups__in=user.groups.all())
+
+        is_self = Q(id=user.id)
+
+        groups = user.groups.all()
+        is_applicant_of_group = Q(application__group__in=groups)
+
+        return get_user_model().objects \
+            .active() \
+            .filter(is_member_of_group | is_applicant_of_group | is_self) \
+            .distinct()
+
+    @staticmethod
+    def get_status(user):
+        return status_data(user)
+
+    @staticmethod
+    def get_places(user):
+        return Place.objects.filter(group__members=user).prefetch_related('subscribers')
+
+    @staticmethod
+    def get_activity_types(user):
+        return ActivityType.objects.filter(group__members=user)
--- karrot/bootstrap/serializers.py
+++ karrot/bootstrap/serializers.py
@@ -1,7 +1,11 @@
 from rest_framework import serializers
 
+from karrot.activities.serializers import ActivityTypeSerializer
 from karrot.groups.serializers import GroupPreviewSerializer
+from karrot.places.serializers import PlaceSerializer
+from karrot.status.helpers import StatusSerializer
 from karrot.userauth.serializers import AuthUserSerializer
+from karrot.users.serializers import UserSerializer
 
 
 class ServerInfoSerializer(serializers.Serializer):
@@ -33,8 +37,15 @@ class ConfigSerializer(serializers.Serializer):
 
 
 class BootstrapSerializer(serializers.Serializer):
-    server = ServerInfoSerializer()
-    config = ConfigSerializer()
-    user = AuthUserSerializer()
-    geoip = GeoSerializer()
-    groups = GroupPreviewSerializer(many=True)
+    # always available
+    server = ServerInfoSerializer(required=False)
+    config = ConfigSerializer(required=False)
+    geoip = GeoSerializer(required=False)
+    groups = GroupPreviewSerializer(many=True, required=False)
+
+    # require authenticated user
+    user = AuthUserSerializer(required=False)
+    places = PlaceSerializer(many=True, required=False)
+    users = UserSerializer(many=True, required=False)
+    status = StatusSerializer(required=False)
+    activity_types = ActivityTypeSerializer(many=True, required=False)
