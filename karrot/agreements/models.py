2b7ecd621287be98db3a1c81fde0e0d69ebce48f
Nick Sellen <git@nicksellen.co.uk>
2022-09-28 15:14:41 +0100

    Add created_by/last_changed_by to agreements

--- karrot/agreements/models.py
+++ karrot/agreements/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 from karrot.base.base_models import BaseModel
@@ -7,25 +8,22 @@ class Agreement(BaseModel):
     group = models.ForeignKey('groups.Group', on_delete=models.CASCADE, related_name='agreements')
 
     title = models.CharField(max_length=240)
-    summary = models.TextField()
+    summary = models.TextField(null=True, blank=True)
     content = models.TextField()
 
     active_from = models.DateTimeField()
     active_until = models.DateTimeField(null=True)
     review_at = models.DateTimeField(null=True)
 
-
-#
-#
-# class AgreementProposal(BaseModel, ConversationMixin):
-#     group = models.ForeignKey('groups.Group', on_delete=models.CASCADE, related_name='agreement_proposals')
-#
-#     title = models.CharField(max_length=240)
-#     summary = models.TextField()
-#     content = models.TextField()
-#
-#     #
-#     agreement = models.OneToOneField(Agreement, on_delete=models.CASCADE, related_name='agreement')
-#
-#     # why somebody proposed it (optional)
-#     reason = models.TextField()
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        related_name='agreements_created',
+        null=True,
+    )
+    last_changed_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        related_name='changed_agreements',
+        null=True,
+    )
--- karrot/agreements/serializers.py
+++ karrot/agreements/serializers.py
@@ -16,6 +16,11 @@ class AgreementSerializer(ModelSerializer):
             'active_until',
             'review_at',
             'group',
+            'created_by',
+            'last_changed_by',
+        ]
+        read_only_fields = [
+            'last_changed_by',
         ]
 
     def validate_group(self, group):
@@ -26,3 +31,10 @@ class AgreementSerializer(ModelSerializer):
         if not group.is_editor(self.context['request'].user):
             raise PermissionDenied('You need to be a group editor')
         return group
+
+    def save(self, **kwargs):
+        current_user = self.context['request'].user
+        extra_kwargs = dict(last_changed_by=current_user)
+        if self.instance is None:
+            extra_kwargs.update(created_by=current_user)
+        return super().save(**kwargs, **extra_kwargs)
