78e418fff92e55608377cc3ef9949cb5bb01055a
Nick Sellen <git@nicksellen.co.uk>
2024-01-03 12:20:56 +0000

    Tweaks

--- karrot/history/migrations/0016_fix_activity_done_missed_history.py
+++ karrot/history/migrations/0016_fix_activity_done_missed_history.py
@@ -26,7 +26,7 @@ def fix_activity_done_missed_history(apps, schema_editor):
         .annotate(
             history_user_count=Count('users', distinct=True),
             activity_user_count=Count('activity__participants', distinct=True),
-        ).filter(activity_user_count__gt=F('history_user_count')):
+        ).filter(activity_user_count__gt=F('history_user_count')).iterator():
         history.users.set(history.activity.participants.all())
 
     # Convert ACTIVITY_MISSED to ACTIVITY_DONE if there were actually participants
@@ -34,7 +34,7 @@ def fix_activity_done_missed_history(apps, schema_editor):
         .filter(typus=HistoryTypus.ACTIVITY_MISSED) \
         .exclude(activity=None) \
         .annotate(activity_user_count=Count('activity__participants', distinct=True)) \
-        .filter(activity_user_count__gt=0):
+        .filter(activity_user_count__gt=0).iterator():
         history.typus = HistoryTypus.ACTIVITY_DONE
         history.save()
         history.users.set(history.activity.participants.all())
