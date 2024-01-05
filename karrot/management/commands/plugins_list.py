9ac60438b02a5c4e50392b5cc78b523aa0080a10
Nick Sellen <git@nicksellen.co.uk>
2024-01-05 12:39:54 +0000

    Fix plugin list command

--- karrot/management/commands/plugins_list.py
+++ karrot/management/commands/plugins_list.py
@@ -1,20 +1,20 @@
 from django.core.management.base import BaseCommand
 
-from karrot.plugins.plugins import list_plugins
+from karrot.plugins.frontend import list_plugins
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         for plugin in list_plugins():
             print(plugin.name)
-            print('  dir:', plugin.dir)
-            print('  asset_dir:', plugin.asset_dir)
-            print('  entry:', plugin.entry)
+            print("  dir:", plugin.dir)
+            print("  asset_dir:", plugin.asset_dir)
+            print("  entry:", plugin.entry)
             if plugin.css_entries:
-                print('  css_entries:')
+                print("  css_entries:")
                 for css_entry in plugin.css_entries:
-                    print('  -', css_entry)
+                    print("  -", css_entry)
             if plugin.assets:
-                print('  assets:')
+                print("  assets:")
                 for asset in plugin.assets:
-                    print('  -', asset)
+                    print("  -", asset)
