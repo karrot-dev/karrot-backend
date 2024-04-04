eb7c3143162563107c07571c6fa68de9bf0b6851
Nick Sellen <git@nicksellen.co.uk>
2024-04-04 22:52:36 +0100

    Revert

--- karrot/meet/api.py
+++ karrot/meet/api.py
@@ -3,7 +3,6 @@ import json
 from django.conf import settings
 from django.db import transaction
 from django.db.models import Q
-from django.views import View
 from rest_framework import status
 from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import ListModelMixin
@@ -41,7 +40,7 @@ class MeetRoomViewSet(
         return super().get_queryset().filter(Q(group__members=self.request.user) | Q(subject_users=self.request.user))
 
 
-class MeetTokenViewSet(View):
+class MeetTokenViewSet(GenericAPIView):
     def get(self, request):
         """Creates a room token for a given room subject
 
